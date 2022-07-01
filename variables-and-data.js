e2f16bd586e193b8103c78ce09b9b3c396768d6e
PaulFore <pfore@cnm.edu>
2022-07-01 13:52:22 -0600

    test

---
+++ variables-and-data.js
@@ -0,0 +1,13 @@
+/*
+ * Variables, Data Taypes, and Typing
+ */
+let wordData;
+
+wordData = "Word list";
+
+console.log(wordData);
+
+let subTotal = 100;
+let tax = subTotal * 0.07;
+let totalWithTax = tax + subTotal;
+console.log(totalWithTax)
